2d06ce4a0fb41a7c7305cdbf852d6d824ba101e3
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-10-26 16:38:17 +0200

    define colors  as  constants in own file

---
+++ src/constants/colors.py
@@ -0,0 +1,12 @@
+# define some colors as tuples
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+BLUE = (255, 0, 0)
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+
+AQUA = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+YELLOW = (0, 255, 255)
