a10afbcc84f4a7b14af2573a0543bc0bc7fb2304
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-10-31 20:13:41 +0100

    new point datatype

---
+++ src/classes/point.py
@@ -0,0 +1,25 @@
+# class for representing one point
+
+class Point:
+    def __init__(self, x_, y_):
+        self.x = x_
+        self.y = y_
+    
+    # unnecessary because variables are public, but use this for cleaner code
+    def setX(self, new_x):
+        self.x = new_x
+
+    def setY(self, new_y):
+        self.y = new_y
+
+    def getPoint(self):
+        return self.__call__
+
+    def __call__(self):
+        return (self.x, self.y)
+
+    def __repr__(self):
+        return str(self.__call__)
+    
+    def __str__(self):
+        return str(self.__call__)
