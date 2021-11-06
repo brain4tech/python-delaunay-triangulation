7c34864c458170fbcb68666b2b392a37ced70905
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-11-06 12:47:14 +0100

    added color variables and methods

--- src/classes/point.py
+++ src/classes/point.py
@@ -1,9 +1,10 @@
 # class for representing one point
 
 class Point:
-    def __init__(self, x_, y_):
+    def __init__(self, x_, y_, color_):
         self.x = x_
         self.y = y_
+        self.color = color_
     
     # unnecessary because variables are public, but use this for cleaner code
     def setX(self, new_x):
@@ -11,6 +12,9 @@ class Point:
 
     def setY(self, new_y):
         self.y = new_y
+    
+    def setColor(self, new_color):
+        self.color = new_color
 
     def getPoint(self):
         return self.__repr__()
