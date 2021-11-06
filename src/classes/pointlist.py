2b46a8a08072c810f302498d90488572c6caf083
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-11-06 21:35:30 +0100

    overhauled pointlist class

--- src/classes/pointlist.py
+++ src/classes/pointlist.py
@@ -8,51 +8,37 @@ class PointList:
     def __init__(self):
         self.__plist: list[Point] = []
 
-    def addPoint(self, point: Point):
+    def append(self, point: Point):
         "Adds a new point to the PointList."
         self.__plist.append(point)
     
-    def getPoints(self):
+    def remove(self, point: Point):
+        self.__plist.remove(point)
+    
+    def me(self):
         """Returns all stored Points in a list."""
-        return self.__plist
+        return self.__plist[:]
     
     def generatePoints(self, minX, maxX, minY, maxY, count, color = (0, 0, 0), padding = 0):
         for _ in range(count):
-            self.addPoint(Point(randint(minX+padding, maxX-padding), randint(minY+padding, maxY-padding), color))
+            self.append(Point(randint(minX + padding, maxX - padding), randint(minY + padding, maxY - padding), color))
     
     # O(n)
-    def getNearestPoint(self, point: Point or tuple) -> Point:
-        """Returns the nearest point to a given tuple of Point."""
+    def getNearestPoint(self, point) -> Point:
+        """Returns the nearest point to a given tuple."""
 
         nearest_point = (0,0)
         nearest_distance = [1000000, 1000000] # use unreachable high number for first comparison
 
-        if isinstance(point, Point):
-            for element in self.__plist:
-                distance_x = abs(element.x - point.x)
-                distance_y = abs(element.y - point.y)
-                if distance_x < nearest_distance[0] and distance_y < nearest_distance[1]:
-                    nearest_distance[0] = distance_x
-                    nearest_distance[1] = distance_y
-                    nearest_point = element
+        for element in self.__plist:
+            distance_x = abs(element.x - point[0])
+            distance_y = abs(element.y - point[1])
+            if distance_x < nearest_distance[0] and distance_y < nearest_distance[1]:
+                nearest_distance[0] = distance_x
+                nearest_distance[1] = distance_y
+                nearest_point = element
 
-
-        elif isinstance(point, tuple):
-                for element in self.__plist:
-                    distance_x = abs(element.x - point[0])
-                    distance_y = abs(element.y - point[1])
-                    if distance_x < nearest_distance[0] and distance_y < nearest_distance[1]:
-                        nearest_distance[0] = distance_x
-                        nearest_distance[1] = distance_y
-                        nearest_point = element
-                
-        else:
-            raise ValueError("Unsupported parameter passed for 'point'")
-        
         return nearest_point
-    
-    def removePoint(self, point: Point):
-        self.__plist.remove(point)
 
 if __name__ == '__main__':
     test_list = PointList()
