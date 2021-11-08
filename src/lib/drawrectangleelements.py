703b6ec5e69166a45acee78c81589fe609a0e5ab
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-11-08 17:28:58 +0100

    new: functions for drawing circumcircle values

--- src/lib/drawrectangleelements.py
+++ src/lib/drawrectangleelements.py
@@ -12,3 +12,9 @@ def drawTrianglePoints(plane, triangle, point_radius):
     pg.draw.circle(plane, addAlpha(triangle.getPointA().color), triangle.getPointA().me(), point_radius)
     pg.draw.circle(plane, addAlpha(triangle.getPointB().color), triangle.getPointB().me(), point_radius)
     pg.draw.circle(plane, addAlpha(triangle.getPointC().color), triangle.getPointC().me(), point_radius)
+
+def drawTriangleCircumcircle(plane, triangle, line_color, line_thickness):
+    pg.draw.circle(plane, addAlpha(line_color), triangle.getCircumcircleCenter(), triangle.getCircumcircleRadius(), line_thickness)
+
+def drawTriangleCircumcircleCenter(plane, triangle, point_color, point_radius):
+    pg.draw.circle(plane, addAlpha(point_color), triangle.getCircumcircleCenter(), point_radius)
