d9e2d1aadbeb71f81eed8471ca3f89a935bdd266
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-11-08 23:19:20 +0100

    give corner points of triangle different color

--- src/lib/drawrectangleelements.py
+++ src/lib/drawrectangleelements.py
@@ -2,16 +2,24 @@
 import pygame as pg
 from lib.misc import addAlpha
 
+import constants.colors as color
 
 def drawTriangleLines(plane, triangle, line_color, line_thickness):
     pg.draw.line(plane, addAlpha(line_color), triangle.getPointA().me(), triangle.getPointB().me(), line_thickness)
     pg.draw.line(plane, addAlpha(line_color), triangle.getPointB().me(), triangle.getPointC().me(), line_thickness)
     pg.draw.line(plane, addAlpha(line_color), triangle.getPointC().me(), triangle.getPointA().me(), line_thickness)
 
+"""
 def drawTrianglePoints(plane, triangle, point_radius):
     pg.draw.circle(plane, addAlpha(triangle.getPointA().color), triangle.getPointA().me(), point_radius)
     pg.draw.circle(plane, addAlpha(triangle.getPointB().color), triangle.getPointB().me(), point_radius)
     pg.draw.circle(plane, addAlpha(triangle.getPointC().color), triangle.getPointC().me(), point_radius)
+"""
+
+def drawTrianglePoints(plane, triangle, point_radius):
+    pg.draw.circle(plane, addAlpha(color.RED), triangle.getPointA().me(), point_radius)
+    pg.draw.circle(plane, addAlpha(color.GREEN), triangle.getPointB().me(), point_radius)
+    pg.draw.circle(plane, addAlpha(color.BLUE), triangle.getPointC().me(), point_radius)
 
 def drawTriangleCircumcircle(plane, triangle, line_color, line_thickness):
     pg.draw.circle(plane, addAlpha(line_color), triangle.getCircumcircleCenter(), triangle.getCircumcircleRadius(), line_thickness)
